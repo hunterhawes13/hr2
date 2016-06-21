676157504cf1e61884619cdc6b112658c66a4ea6
Hunter Hawes <hunterhawes13@gmail.com>
2016-06-21 09:48:01 -0500

    horse race challenge tuesday week 2

---
+++ stakes.js
@@ -0,0 +1,17 @@
+var horseRace = {
+  title:"Belmont Stakes", 
+  date:"June 11 2016", 
+  attendees:"50,000", 
+  location: {
+    streetAddress: "2150 Hempstead Turnpike",
+    city: "Brooklyn",
+    state: "NY",
+    zipcode: "11003",
+    },
+    
+horses: [1,2,3,4,5,6,7,8,9],
+race: [1,2,3],
+raceCard:  [[1,[5,8,2]],[2,[1,3,9]],[3,[7,4,6]]],
+              
+              };
+console.log(horseRace.title);
